18a771115e60159f9641b106379145d649756c73
Илья <ilyagavrilov@mail.ru>
2020-11-27 11:45:11 +0300

    commit

--- lesson3-spring-mvc/src/main/java/ru/geekbrains/AppConfig.java
+++ lesson3-spring-mvc/src/main/java/ru/geekbrains/AppConfig.java
@@ -1,5 +1,6 @@
 package ru.geekbrains;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
